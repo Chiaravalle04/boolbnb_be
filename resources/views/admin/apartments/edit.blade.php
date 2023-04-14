9df31481175d00cf5df512d883c0f39ea3902a2f
Fedois <fedefox14@gmail.com>
2023-04-14 17:45:24 +0200

    aggiunto errors in edit apartment

--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -7,7 +7,18 @@
                 <h1>
                     Modifica Appartamento
                 </h1>
+                
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
             </div>
+            
         </div>
         <div class="row mb-4">
             <div class="col">
