5db20e814e4e8bc74c26e8e16152be0ca2b88be8
Fedois <fedefox14@gmail.com>
2023-04-14 15:46:41 +0200

    aggiunto tabella

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,4 +1,36 @@
 @extends('layouts.admin')
 @section('content')
-    
+    <div class="container">
+        <div class="title my-3">
+            <h1>Apartments</h1>
+        </div>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">id</th>
+                <th scope="col">title</th>
+                <th scope="col">address</th>
+                <th scope="col">room</th>
+                <th scope="col">price</th>
+              </tr>
+            </thead>
+            
+            <tbody>
+                @foreach ($apartments as $apartment)
+                <tr>
+                    <th scope="row">{{ $apartment->id }}</th>
+                    <td>{{ $apartment->title }}</td>
+                    <td>{{ $apartment->address }}</td>
+                    <td>{{ $apartment->room }}</td>
+                    <td>{{ $apartment->price }}</td>
+                    <td>
+                        <a href="{{ route('admin.apartments.show', $apartment->id) }}" class="btn btn-outline-primary border">
+                            visualizza
+                        </a>
+                    </td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+    </div>
 @endsection
