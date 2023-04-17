b94bb234054f8eb10538ad7ec55e3e134deeda74
Step1017 <s.aveta@yahoo.com>
2023-04-17 10:49:11 +0200

    Fatto edit services

--- resources/views/admin/services/edit.blade.php
+++ resources/views/admin/services/edit.blade.php
@@ -1,4 +1,54 @@
 @extends('layouts.admin')
 @section('content')
-    
+    <div class="container-fluid mt-4">
+        <div class="row justify-content-center mb-4">
+            <div class="col">
+                <h1>
+                    Modifica Servizio
+                </h1>
+                
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
+            
+        </div>
+        <div class="row mb-4">
+            <div class="col">
+                <form action="{{ route('admin.services.update', $service->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nome<span class="text-danger">*</span></label>
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            id="name"
+                            name="name"
+                            required
+                            maxlength="128"
+                            value="{{ old('name', $service->name) }}" 
+                            placeholder="Inserisci il nome del servizio...">
+                    </div>
+                    <div>
+                        <p>
+                            N.B. i campi contrassegnati con <span class="text-danger">*</span> sono obbligatori.
+                        </p>
+                    </div>
+                    <div class="btn-box mt-4">
+                        <a href="{{ route('admin.services.index') }}" class="btn btn-warning text-light">
+                            <i class="fa-solid fa-rotate-left"></i>
+                        </a>
+                        <button type="submit" class="btn btn-success">Modifica</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
