dd0b120c0b860f42cbff728f9328f2fad1f11164
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-04-19 19:02:41 +0200

    Cambiato ordine di app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,14 +2,10 @@ import './bootstrap';
 
 import '~resources/scss/app.scss';
 
-// import axios from 'axios';
-
-import './autocomplete.js'
-
 import * as bootstrap from 'bootstrap';
 
 import.meta.glob([
     '../img/**'
 ]);
 
-
+import './autocomplete.js'
