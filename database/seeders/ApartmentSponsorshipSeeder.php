6784824999a1a12c7f3a105ac85d6b4648707a49
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-05-01 11:18:00 +0200

    aggiunto seeder sponsor

---
+++ database/seeders/ApartmentSponsorshipSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Apartment;
+use App\Models\Sponsorship;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class ApartmentSponsorshipSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $apartments = Apartment::all();
+
+        $sponsorships = Sponsorship::all()->pluck('id');
+
+        foreach ($faker->randomElements($apartments, 8) as $apartment) {
+            $apartment->sponsorships()->attach($faker->randomElement($sponsorships), [
+                'expired_date' => Carbon::now()->addDays(6)
+            ]);
+        }
+
+        // $otherApartments = Apartment::where('address', 'LIKE', '%' . 'Roma')->orWhere('address', 'LIKE', '%' . 'Torino' . '%')->get();
+
+        // foreach ($otherApartments as $apartment) {
+        //     $apartment->sponsorships()->attach($faker->randomElement($sponsorships), [
+        //         'starting_date' => $faker->dateTime(),
+        //         'ending_date' => Carbon::now()->addDays(6)
+        //     ]);
+        // }
+    }
+}
