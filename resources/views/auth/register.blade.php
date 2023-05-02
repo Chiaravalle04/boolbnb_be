e41a716f1d00355db80a6976a6a5464317c35c57
Step1017 <s.aveta@yahoo.com>
2023-05-02 13:02:38 +0200

    - Modifica stile form registrazione - Cambiata lingua label input

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-4">
+<div class="container-login container mt-4">
     <div class="row-login row justify-content-center align-items-center">
         <div class="box-shadow border-radius-15 card col-5">
 
             <div class="card-body">
+                <div class="container-img-login text-center">
+
+                    {{-- <img class="my-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1200px-Airbnb_Logo_B%C3%A9lo.svg.png" alt=""> --}}
+                    <h1 class="title-bool">BoolBnb</h1>
+
+                </div>
                 <form method="POST" action="{{ route('register') }}">
                     @csrf
 
                     <div class="mb-2 row">
                         <div>
-                            <label for="name" class="my-label col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="my-label col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
                         </div>
 
                         <div class="col">
@@ -27,7 +33,7 @@
 
                     <div class="mb-2 row">
                         <div>
-                            <label for="surname" class="my-label col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
+                            <label for="surname" class="my-label col-md-4 col-form-label text-md-right">{{ __('Cognome') }}</label>
                         </div>
                         <div class="col">
                             <input id="surname" type="text" class="my-input @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" autocomplete="surname" autofocus>
@@ -42,7 +48,7 @@
 
                     <div class="mb-2 row">
                         <div>
-                            <label for="birth_date" class="my-label col-md-4 col-form-label text-md-right">{{ __('Birth date') }}</label>
+                            <label for="birth_date" class="my-label col-md-4 col-form-label text-md-right">{{ __('Data di nascita') }}</label>
                         </div>
                         <div class="col">
                             <input id="birth_date" type="date" class="my-input @error('birth_date') is-invalid @enderror" name="birth_date" value="{{ old('birth_date') }}" autocomplete="birth_date" autofocus>
@@ -57,7 +63,7 @@
 
                     <div class="mb-2 row">
                         <div>
-                            <label for="email" class="my-label col-md-4 col-form-label text-md-right">{{ __('E-Mail Address *') }}</label>
+                            <label for="email" class="my-label col-md-4 col-form-label text-md-right">{{ __('E-Mail *') }}</label>
                         </div>
                         <div class="col">
                             <input id="email" type="email" class="my-input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -87,7 +93,7 @@
 
                     <div class="mb-2 row">
                         <div>
-                            <label for="password-confirm" class="my-label col-md-4 col-form-label text-md-right">{{ __('Confirm Password *') }}</label>
+                            <label for="password-confirm" class="my-label col-md-4 col-form-label text-md-right">{{ __('Conferma Password *') }}</label>
                         </div>
                         <div class="col">
                             <input id="password-confirm" type="password" class="my-input" name="password_confirmation" required autocomplete="new-password">
@@ -96,8 +102,8 @@
 
                     <div class="mt-3 row mb-0">
                         <div class="col-md-6">
-                            <button type="submit" class="btn-login text-center">
-                                {{ __('Register') }}
+                            <button type="submit" class="btn-login text-center w-50">
+                                {{ __('Registrati') }}
                             </button>
                         </div>
                     </div>
